2f97660231cf7274086642687914b41eaac181d9
Max Moskalchuk <gmxiiim@gmail.com>
2021-02-03 22:41:55 +0200

    Implement getting of context for Canvas2D

--- src/lib/classes/Canvas2D.ts
+++ src/lib/classes/Canvas2D.ts
@@ -1,7 +1,15 @@
 import { Canvas } from './Canvas';
 
 export class Canvas2D extends Canvas {
+  private context: CanvasRenderingContext2D;
+
   constructor(id: string) {
     super(id);
+
+    const context = this.canvas.getContext('2d');
+
+    if (!context) throw 'Canvas is set to a different context mode';
+
+    this.context = context;
   }
 }
