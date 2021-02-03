9c6713f619dbde619658db2fcbb2d830f7818c14
Max Moskalchuk <gmxiiim@gmail.com>
2021-02-03 22:16:50 +0200

    Create a base constructor

--- src/lib/classes/Canvas.ts
+++ src/lib/classes/Canvas.ts
@@ -1,4 +1,14 @@
 export class Canvas {
+  protected canvas: HTMLCanvasElement;
+
+  constructor(id: string) {
+    const element: HTMLElement | null = document.getElementById(id);
+
+    if (!element || !Canvas.isCanvasElement(element)) throw 'Invalid ID';
+
+    this.canvas = element;
+  }
+
   static isCanvasElement(element: HTMLElement): element is HTMLCanvasElement {
     return element.nodeName === 'CANVAS';
   }
--- src/lib/classes/Canvas2D.ts
+++ src/lib/classes/Canvas2D.ts
@@ -1,3 +1,7 @@
 import { Canvas } from './Canvas';
 
-export class Canvas2D extends Canvas {}
+export class Canvas2D extends Canvas {
+  constructor(id: string) {
+    super(id);
+  }
+}
